c732534ab435fe4fc9064b9034f9e289bfc9e690
jaime ambriz <jaime.ambriz@yahoo.com>
2017-11-05 04:27:46 -0700

    first commit

---
+++ app/roster/players-controller.js
@@ -0,0 +1,98 @@
+function PlayersController() {
+
+    var loading = true; //Start the spinner
+    var playersService = new PlayersService(ready);
+
+    function ready() {
+        loading = false; //stop the spinner
+
+        
+    }
+    this.getPlayersByName = function getPlayersByName(e) {
+        e.preventDefault();
+        var player = e.target.player.value;
+        playersService.getPlayersByName(player)
+        updateRoster(playersService.getPlayers()) 
+    }
+
+    this.getPlayersByTeam = function getPlayersByTeam(e) {
+        e.preventDefault();
+        var team = e.target.team.value;
+        playersService.getPlayersByTeam(team)
+        updateRoster(playersService.getPlayers()) 
+    }
+
+    // this.getPlayersByTeam = function getPlayersByTeam(e) {
+    //     e.preventDefault();
+    //     var team = e.target.team.value;
+    //     updateRoster(playersService.getPlayersByTeam(team)) 
+    // }
+
+     this.getPlayersByPosition = function getPlayersByPosition(e) {
+        e.preventDefault();
+        var position = e.target.position.value;
+        playersService.getPlayersByPosition(position)
+        updateRoster(playersService.getPlayers()) 
+      }
+
+
+    function updateRoster(players) {
+        var template = ""
+        for (var i = 0; i < players.length; i++) {
+            var player = players[i];
+            template += `
+          <div class="player-card">
+          <img src="${player.photo}" alt="">
+          <h1>${player.fullname}</h1>
+          <h2>${player.position}</h2>
+          <h3>${player.pro_team}, ${player.jersey}</h3>
+          <button class="btn-success" id="${player.id}" onclick="app.controllers.playersController.add('${player.id}')">Add to Team</button>
+      </div>
+          `
+
+        }
+        document.getElementById("player-roster").innerHTML = template
+    }
+
+    function updateMyTeam(players) {
+        var template = ""
+        for (var i = 0; i < players.length; i++) {
+            var player = players[i];
+            template += `
+          <div class="player-card">
+          <img src="${player.photo}" alt="">
+          <h1>${player.fullname}</h1>
+          <h2>${player.position}</h2>
+          <h3>${player.pro_team}, ${player.jersey}</h3>
+          <button class="btn-danger" id="${player.id}" onclick="app.controllers.playersController.remove('${player.id}')">Remove From Team</button>
+      </div>
+          `
+
+        }
+        document.getElementById("my-team").innerHTML = template
+    }
+
+    this.add = function add(id) {
+        playersService.addToTeam(id)
+        updateRoster(playersService.getPlayers())
+        updateMyTeam(playersService.getMyTeam())
+    }
+
+    this.remove = function remove(id) {
+        playersService.removeFromTeam(id)
+        updateRoster(playersService.getPlayers())
+        updateMyTeam(playersService.getMyTeam())
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/roster/players-service.js
@@ -0,0 +1,211 @@
+function PlayersService(callback) {
+    var playersData = [];
+    var myTeam = [];
+    var players = []
+
+    this.getPlayers = function () {
+        return JSON.parse(JSON.stringify(players))
+    }
+
+    this.getMyTeam = function () {
+        return (JSON.parse(JSON.stringify(myTeam)))
+    }
+
+    this.getPlayersByName = function (name) {
+        players = []
+        return playersData.filter(function (player) {
+            if (player.firstname.toLowerCase() == name.toLowerCase()) {
+                if (player.jersey !== undefined) {
+                    if (player.photo !== "http://sports.cbsimg.net/images/players/unknown-player-170x170.png") {
+                        players.push(player);
+                    }
+                }
+            }
+        });
+        return players
+    }
+
+    this.getPlayersByTeam = function (teamName) {
+        var teams = [{
+            name: "seahawks",
+            city: "seattle",
+            abbreviation: "sea"}, {
+            name: "patriots",
+            city: "new england",
+            abbreviation: "ne"}, {
+            name: "cardinals",
+            city: "arizona",
+            abbreviation: "ari"}, {
+            name: "falcons",
+            city: "atlanta",
+            abbreviation: "atl"}, {
+            name: "ravens",
+            city: "baltimore",
+            abbreviation: "bal"}, {
+            name: "bills",
+            city: "buffalo",
+            abbreviation: "buf"}, {
+            name: "panthers",
+            city: "carolina",
+            abbreviation: "car"}, {
+            name: "bears",
+            city: "chicago",
+            abbreviation: "chi"}, {
+            name: "bengals",
+            city: "cincinnati",
+            abbreviation: "cin"}, {
+            name: "cowboys",
+            city: "dallas",
+            abbreviation: "dal"}, {
+            name: "broncos",
+            city: "denver",
+            abbreviation: "den"}, {
+            name: "lions",
+            city: "detroit",
+            abbreviation: "det"}, {
+            name: "packers",
+            city: "green bay",
+            abbreviation: "gb"}, {
+            name: "texans",
+            city: "huston",
+            abbreviation: "hou"}, {
+            name: "colts",
+            city: "indianapolis",
+            abbreviation: "ind"}, {
+            name: "jaguars",
+            city: "jacksonville",
+            abbreviation: "jax"}, {
+            name: "chiefs",
+            city: "kansas city",
+            abbreviation: "kc"}, {
+            name: "vikings",
+            city: "minnesota",
+            abbreviation: "min"}, {
+            name: "saints",
+            city: "new orleans",
+            abbreviation: "no"}, {
+            name: "giants",
+            city: "new york",
+            abbreviation: "nyg"}, {
+            name: "jets",
+            city: "new york",
+            abbreviation: "nyj"}, {
+            name: "raiders",
+            city: "oakland",
+            abbreviation: "oak"}, {
+            name: "eagles",
+            city: "philadelphia",
+            abbreviation: "phi"}, {
+            name: "steelers",
+            city: "pitssburgh",
+            abbreviation: "pit"}, {
+            name: "chargers",
+            city: "los asngeles",
+            abbreviation: "lac"}, {
+            name: "rams",
+            city: "los angeles",
+            abbreviation: "lar"}, {
+            name: "49ers",
+            city: "san fransisco",
+            abbreviation: "sf"}, {
+            name: "buccaneers",
+            city: "tampa bay",
+            abbreviation: "tb"}, {
+            name: "titans",
+            city: "tennessee",
+            abbreviation: "ten"}, {
+            name: "redskins",
+            city: "washington",
+            abbreviation: "was"}]
+        var teamName = teamName.toLowerCase()
+        players = []
+        var out = ""
+        for (var i = 0; i < teams.length; i++) {
+            var team = teams[i];
+            if (team.name == teamName || team.city == teamName || team.abbreviation == teamName) {
+                out = team.abbreviation
+            }
+        }
+        return playersData.filter(function (player) {
+            if (player.pro_team.toLowerCase() == out) {
+                if (player.jersey !== undefined) {
+                    if (player.photo !== "http://sports.cbsimg.net/images/players/unknown-player-170x170.png") {
+                        players.push(player);
+                    }
+                }
+            }
+        });
+    }
+
+    // this.getPlayersByTeam = function (team) {
+    //     return playersData.filter(function (player) {
+    //         if (player.pro_team == team) {
+
+    //             return true;
+    //         }
+    //     });
+    // }
+
+    this.getPlayersByPosition = function (position) {
+        players = []
+        return playersData.filter(function (player) {
+            if (player.position.toLowerCase() == position.toLowerCase()) {
+                if (player.jersey !== undefined) {
+                    if (player.photo !== "http://sports.cbsimg.net/images/players/unknown-player-170x170.png") {
+                        players.push(player);
+                    }
+                }
+            }
+        });
+        return players
+    }
+
+    this.addToTeam = function (id) {
+        for (var i = 0; i < players.length; i++) {
+            var player = players[i];
+            if (id == player.id && myTeam.length < 9) {
+                myTeam.push(player)
+                players.splice(i, 1)
+            }
+        }
+    }
+    this.removeFromTeam = function (id) {
+        for (var i = 0; i < myTeam.length; i++) {
+            var player = myTeam[i];
+            if (id == player.id) {
+                players.push(player)
+                myTeam.splice(i, 1)
+            }
+        }
+    }
+
+    function loadPlayersData() {
+
+        //Lets check the localstorage for the data before making the call.
+        //Ideally if a user has already used your site 
+        //we can cut down on the load time by saving and pulling from localstorage 
+        var localData = localStorage.getItem('playersData');
+        if (localData) {
+            playersData = JSON.parse(localData);
+            return callback();
+            //return will short-circuit the loadPlayersData function
+            //this will prevent the code below from ever executing
+        }
+
+        var url = "https://bcw-getter.herokuapp.com/?url=";
+        var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+        var apiUrl = url + encodeURIComponent(endpointUri);
+
+        $.getJSON(apiUrl, function (data) {
+            playersData = data.body.players;
+            console.log('Player Data Ready')
+            console.log('Writing Player Data to localStorage')
+            localStorage.setItem('playersData', JSON.stringify(playersData))
+            console.log('Finished Writing Player Data to localStorage')
+            callback()
+            console.log(playersData)
+        });
+    }
+    loadPlayersData(); //call the function above every time we create a new service
+    console.log(playersData)
+}
